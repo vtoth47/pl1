f04d42e0053553d85e29d9475ec4fb26e3119f47
vtoth47 <91272846+vtoth47@users.noreply.github.com>
2022-01-25 13:36:26 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 49.782, lng: -85.909 },
     zoom: 8,
   });
 }

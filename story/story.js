f30b2b30b613b09159774e8c731c6508a8f1c5fd
vtoth47 <91272846+vtoth47@users.noreply.github.com>
2022-01-31 17:15:47 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -5,31 +5,48 @@ const map = new google.maps.Map(document.getElementById("map"), {
       mapTypeId: 'hybrid',
 });
 
-new google.maps.Marker({
+const marker1 = new google.maps.Marker({
   position: { lat: 43.402192, lng: -80.354693},
   map,
   label: "1",
   });
-new google.maps.Marker({
+const marker2 = new google.maps.Marker({
   position: { lat: 43.543167, lng: -79.694334},
   map,
   label: "2",
   });
-new google.maps.Marker({
+const marker3 = new google.maps.Marker({
   position: { lat: 43.679481, lng: -79.625636},
   map,
   label: "3",
   });
-new google.maps.Marker({
+const marker4 = new google.maps.Marker({
   position: { lat: 48.569819, lng: -81.374618},
   map,
   label: "4",
   }); 
- new google.maps.Marker({
+const marker5 =  new google.maps.Marker({
   position: { lat: 52.818330, lng: -83.907099},
   map,
   label: "5",
   }); 
+
+const contentString = 
+      "<h1>Home</h1>";
+      
+const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+});      
+      
+ marker.addListener("click", () => {
+    infowindow.open({
+      anchor: marker,
+      map,
+      shouldFocus: false,
+    });
+  });
+
+
 }
 
 

d649c04fe58d9c6eb4a582f564a676fe98bbf49b
vtoth47 <91272846+vtoth47@users.noreply.github.com>
2022-02-02 13:33:46 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -27,7 +27,7 @@ function initMap() {
       label: "2",
       });
     var infowindow2 = new google.maps.InfoWindow({
-        content: "<h2>Parents</h2><p>My former home, and also where I would drop off my dog for the 2 weeks I was away at work</p>"
+        content: "<h2>Parents</h2><p>My former home, and also where I would drop off my dog for the 2 weeks I was away at work</p><img src='milo.jpg' alt='Milo'>"
     });  
     google.maps.event.addListener(marker2, 'click', function() {
         infowindow2.open(map,marker2);
@@ -40,7 +40,7 @@ function initMap() {
       label: "3",
       });
     var infowindow3 = new google.maps.InfoWindow({
-        content: "<h2>Pearson Airport</h2><p>I would typically catch the 8:45AM flight from YYZ to YTS aboard an Air Canada Dash 8.</p><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Air_Canada_Express_De_Havilland_Canada_DHC-8-301_Dash_8_C-GKTA_2_%288027593421%29.jpg/640px-Air_Canada_Express_De_Havilland_Canada_DHC-8-301_Dash_8_C-GKTA_2_%288027593421%29.jpg' alt='Dash8'>"
+        content: "<h2>Pearson Airport - YYZ</h2><p>I would typically catch the 8:45AM flight from YYZ to YTS aboard an Air Canada Dash 8.</p><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Air_Canada_Express_De_Havilland_Canada_DHC-8-301_Dash_8_C-GKTA_2_%288027593421%29.jpg/640px-Air_Canada_Express_De_Havilland_Canada_DHC-8-301_Dash_8_C-GKTA_2_%288027593421%29.jpg' alt='Dash8'>"
     });  
     google.maps.event.addListener(marker3, 'click', function() {
         infowindow3.open(map,marker3);
@@ -53,7 +53,7 @@ function initMap() {
       label: "4",
       }); 
     var infowindow4 = new google.maps.InfoWindow({
-        content: "<div class=infowindow><h1>Timmins Airport</h1></div>"
+        content: "<h2>Timmins Airport - YTS</h2><p>Arriving around noon, employees would wait at the airport for our flight to the mine</p><br><p>One day I was lucky enough to meet LIGHTS at the shady airport diner.</p><img src='Lights_YTS.jpg' alt='Lights'>"
     });  
     google.maps.event.addListener(marker4, 'click', function() {
         infowindow4.open(map,marker4);
@@ -66,7 +66,7 @@ function initMap() {
       label: "5",
       }); 
     var infowindow5 = new google.maps.InfoWindow({
-        content: "<div class=infowindow><h1>The Mine</h1></div>"
+        content: "<h2>The Mine</h2><p>Ontario's first and only open-pit diamond mine.</p><br><p>The Victor Mine was also the best mine and had the best people I've worked with in my mining career.<img src='victormine.jpg' alt='Victor Mine'>"
     });  
     google.maps.event.addListener(marker5, 'click', function() {
         infowindow5.open(map,marker5);
